de69c92693f587dcec78c97ab2ac21fae2baf495
Laura Doktorova <ldoktorova@gmail.com>
2011-05-31 10:15:34 -0400

    re-indent only, no changes

--- benchmarks/templating/doT.js
+++ benchmarks/templating/doT.js
@@ -10,7 +10,7 @@
 // Licensed under the MIT license.
 //
 (function() {
-	var doT = { version : '0.1.4' };
+	var doT = { version : '0.1.5' };
 
 	if (typeof module !== 'undefined' && module.exports) {
 		module.exports = doT;
@@ -31,41 +31,41 @@
 
 	function resolveDefs(define, use, str, defs) {
 		return str.replace(define, function (match, code, value) {
-				if (!(code in defs)) defs[code]=value;
-				return '';
-			})
-			.replace(use, function(match, code) {
-				var value;// todo: detect circular use and convert into compiled functions
-				with(defs) {try { value = eval(code);} catch(e) { value='';} }
-				return value ? resolveDefs(define, use, value.toString(), defs) : value;
-			});
+			if (!(code in defs)) defs[code]=value;
+			return '';
+		})
+		.replace(use, function(match, code) {
+			var value; // todo: detect circular use and convert into compiled functions
+			with(defs) {try { value = eval(code);} catch(e) { value='';} }
+			return value ? resolveDefs(define, use, value.toString(), defs) : value;
+		});
 	}
 
 	doT.template = function(tmpl, c, defs) {
 		c = c || doT.templateSettings;
-		var cstart = c.append ? "'+(" : "';out+=(",// optimal choice depends on platform/size of templates
+		var cstart = c.append ? "'+(" : "';out+=(", // optimal choice depends on platform/size of templates
 		    cend   = c.append ? ")+'" : ");out+='";
 		var str = (c.use || c.define) ? resolveDefs(c.define, c.use, tmpl, defs || {}) : tmpl;
 
 		str = ("var out='" +
-				((c.strip) ? str.replace(/\s*<!\[CDATA\[\s*|\s*\]\]>\s*|[\r\n\t]|(\/\*[\s\S]*?\*\/)/g, ''): str)
-				.replace(/\\/g, '\\\\')
-				.replace(/'/g, "\\'")
-				.replace(c.interpolate, function(match, code) {
-					return cstart + code.replace(/\\'/g, "'").replace(/\\\\/g,"\\").replace(/[\r\t\n]/g, ' ') + cend;
-				})
-				.replace(c.encode, function(match, code) {
-					return cstart + code.replace(/\\'/g, "'").replace(/\\\\/g, "\\").replace(/[\r\t\n]/g, ' ') + ").toString().replace(/&(?!\\w+;)/g, '&#38;').split('<').join('&#60;').split('>').join('&#62;').split('" + '"' + "').join('&#34;').split(" + '"' + "'" + '"' + ").join('&#39;').split('/').join('&#x2F;'" + cend;
-				})
-				.replace(c.evaluate, function(match, code) {
-					return "';" + code.replace(/\\'/g, "'").replace(/\\\\/g,"\\").replace(/[\r\t\n]/g, ' ') + "out+='";
-				})
-				+ "';return out;")
-				.replace(/\n/g, '\\n')
-				.replace(/\t/g, '\\t')
-				.replace(/\r/g, '\\r')
-				.split("out+='';").join('')
-				.split('var out="";out+=').join('var out=');
+			((c.strip) ? str.replace(/\s*<!\[CDATA\[\s*|\s*\]\]>\s*|[\r\n\t]|(\/\*[\s\S]*?\*\/)/g, ''): str)
+			.replace(/\\/g, '\\\\')
+			.replace(/'/g, "\\'")
+			.replace(c.interpolate, function(match, code) {
+				return cstart + code.replace(/\\'/g, "'").replace(/\\\\/g,"\\").replace(/[\r\t\n]/g, ' ') + cend;
+			})
+			.replace(c.encode, function(match, code) {
+				return cstart + code.replace(/\\'/g, "'").replace(/\\\\/g, "\\").replace(/[\r\t\n]/g, ' ') + ").toString().replace(/&(?!\\w+;)/g, '&#38;').split('<').join('&#60;').split('>').join('&#62;').split('" + '"' + "').join('&#34;').split(" + '"' + "'" + '"' + ").join('&#39;').split('/').join('&#x2F;'" + cend;
+			})
+			.replace(c.evaluate, function(match, code) {
+				return "';" + code.replace(/\\'/g, "'").replace(/\\\\/g,"\\").replace(/[\r\t\n]/g, ' ') + "out+='";
+			})
+			+ "';return out;")
+			.replace(/\n/g, '\\n')
+			.replace(/\t/g, '\\t')
+			.replace(/\r/g, '\\r')
+			.split("out+='';").join('')
+			.split('var out="";out+=').join('var out=');
 
 		try {
 			return new Function(c.varname, str);
