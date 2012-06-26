2622024bd41d48573f2cd41bb3140bd0dce3d43e
Laura Doktorova <ldoktorova@gmail.com>
2012-06-26 01:37:39 -0400

    encoding fix

--- doT.js
+++ doT.js
@@ -38,7 +38,7 @@
 
 	function encodeHTMLSource() {
 		var encodeHTMLRules = { "&": "&#38;", "<": "&#60;", ">": "&#62;", '"': '&#34;', "'": '&#39;', "/": '&#47;' },
-			matchHTML = /&(?!\\w+;)|<|>|"|'|\//g;
+			matchHTML = /&(?!#?\w+;)|<|>|"|'|\//g;
 		return function(code) {
 			return code ? code.toString().replace(matchHTML, function(m) {return encodeHTMLRules[m] || m;}) : code;
 		};
