3b796d91dd95feed470b6d94d6f5f3df27fe1831
Joel Purra <code+github@joelpurra.com>
2014-02-12 22:09:59 +0100

    Tests to show that square bracket properties do not work

--- test/testdoT.js
+++ test/testdoT.js
@@ -27,4 +27,55 @@ describe('doT', function(){
 		   assert.equal("<div></div>", definescompiled({}));
 		});
 	});
+
+	describe('defines with parameter', function(){
+		var compiledDefinesParamTemplate = function(param){
+			var definesparamtemplate = "{{##def.tmp:input:<div>{{!input.foo}}</div>#}}{{#def.tmp:" + param + "}}",
+				definesparamcompiled = doT.template(definesparamtemplate);
+
+				return definesparamcompiled;
+			};
+
+		it('should render define with standard parameter', function(){
+			var definesparamcompiled = compiledDefinesParamTemplate("it");
+
+		   assert.equal(definesparamcompiled({foo:"A"}), "<div>A</div>");
+		   assert.equal(definesparamcompiled({}), "<div></div>");
+		});
+
+		it('should render define with property parameter', function(){
+			var definesparamcompiled = compiledDefinesParamTemplate("it.bar");
+
+		   assert.equal(definesparamcompiled({bar:{foo:"B"}}), "<div>B</div>");
+		   assert.throws(function() { definesparamcompiled({}); }, /TypeError: Cannot read property 'foo' of undefined/);
+		});
+
+		it('should render define with square bracket property parameter', function(){
+			var definesparamcompiled = compiledDefinesParamTemplate("it['bar']");
+
+		   assert.equal(definesparamcompiled({bar:{foo:"C"}}), "<div>C</div>");
+		   assert.throws(function() { definesparamcompiled({}); }, /TypeError: Cannot read property 'foo' of undefined/);
+		});
+
+		it('should render define with square bracket property with space parameter', function(){
+			var definesparamcompiled = compiledDefinesParamTemplate("it['bar baz']");
+
+		   assert.equal(definesparamcompiled({"bar baz":{foo:"D"}}), "<div>D</div>");
+		   assert.throws(function() { definesparamcompiled({}); }, /TypeError: Cannot read property 'foo' of undefined/);
+		});
+
+		it('should render define with array index property parameter', function(){
+			var definesparamcompiled = compiledDefinesParamTemplate("it[1]");
+
+		   assert.equal(definesparamcompiled(["not this", {foo:"E"}, "not this"]), "<div>E</div>");
+		   assert.throws(function() { definesparamcompiled({}); }, /TypeError: Cannot read property 'foo' of undefined/);
+		});
+
+		it('should render define with deep properties parameter', function(){
+			var definesparamcompiled = compiledDefinesParamTemplate("it['bar baz'].qux[1]");
+
+		   assert.equal(definesparamcompiled({"bar baz":{qux:["not this", {foo:"F"}, "not this"]}}), "<div>F</div>");
+		   assert.throws(function() { definesparamcompiled({}); }, /TypeError: Cannot read property 'qux' of undefined/);
+		});
+	});
 });
