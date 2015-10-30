6fae6a64814852ff055a0dae3c5985c281a0985d
淘小杰 <hustcoolboy@gmail.com>
2015-10-30 09:39:05 +0800

    fix  encodeHTMLSource  when code = 0
    when encodeHTMLSource got a parameter  code = 0,  encodeHTMLSource will output  ''  , but the expected  output  is
    '0'

--- doT.js
+++ doT.js
@@ -31,7 +31,7 @@
 		var encodeHTMLRules = { "&": "&#38;", "<": "&#60;", ">": "&#62;", '"': "&#34;", "'": "&#39;", "/": "&#47;" },
 			matchHTML = doNotSkipEncoded ? /[&<>"'\/]/g : /&(?!#?\w+;)|<|>|"|'|\//g;
 		return function(code) {
-			return code ? code.toString().replace(matchHTML, function(m) {return encodeHTMLRules[m] || m;}) : "";
+			return code !== undefined ? code.toString().replace(matchHTML, function(m) {return encodeHTMLRules[m] || m;}) : "";
 		};
 	};
 
